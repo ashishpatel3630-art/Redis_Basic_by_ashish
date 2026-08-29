5252c2fedb74de65bd6ef52cbd70c0f9682a88ab
Ashish Patel <ashishpatel3630@gmail.com>
2026-08-29 23:27:58 +0530

    user profile with redis and hash

--- 03_site_banner/src/index.js
+++ 03_site_banner/src/index.js
@@ -1,43 +1,83 @@
-import express from "express"
-import Redis from "ioredis"
+import express from "express";
+import Redis from "ioredis";
 
 const app = express();
+
 app.use(express.json());
 
-
-const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
+const redis = new Redis(
+  process.env.REDIS_URL || "redis://localhost:6379"
+);
 
 const BANNER_KEY = "app:banner";
 
-app.post("/banner" , async (req ,  res)=>{
-    await redis.set(BANNER_KEY , req.body.message || "welcome to chai or redis  ");
-    res.json({ success:true   
-    });
 
-});
+app.post("/banner", async (req, res) => {
+  try {
+    const message = req.body.message || "Welcome to Chai aur Redis";
 
-app.get("/banner" , async (req , res)=>{
-    const message = await redis.get (BANNER_KEY);
+    await redis.set(BANNER_KEY, message);
+
     res.json({
-        message
+      success: true,
+      message,
     });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
 
-app.delete("/banner" , async(req , res)=>{
-    await redis.delete(BANNER_KEY);
+app.get("/banner", async (req, res) => {
+  try {
+    const message = await redis.get(BANNER_KEY);
+
     res.json({
-        message
+      message,
     });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
-app.get("/banner/exists" , async( req , res)=>{
+
+app.delete("/banner", async (req, res) => {
+  try {
+    await redis.del(BANNER_KEY);
+
+    res.json({
+      success: true,
+      message: "Banner deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+app.get("/banner/exists", async (req, res) => {
+  try {
     const exists = await redis.exists(BANNER_KEY);
+
     res.json({
-        exists:Boolean(exists)
+      exists: Boolean(exists),
     });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
-app.listen(3000,()=>{
-    console.log("server running on port https://localhost:3000")
-})
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
