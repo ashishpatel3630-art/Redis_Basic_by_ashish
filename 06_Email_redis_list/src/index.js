ae4d3bd4ab3fbd3b5678a867ae959f23479be171
Ashish Patel <ashishpatel3630@gmail.com>
2026-08-30 23:47:30 +0530

    email is setup

--- 06_Email_redis_list/src/index.js
+++ 06_Email_redis_list/src/index.js
@@ -1,7 +1,21 @@
 import express from "express"
+import { body } from "framer-motion/client";
 import Redis from "ioredis"
 
 const app = express();
 app.use(express.json())
 
 const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379" );
+
+const QUEUE_KEY = 'queue:emails';
+
+app.post("/emails" , async( req , res)=>{
+    const job ={
+        to : req.body.to,
+        subject : req.body.subject || "No Subject",
+        body: req.body.body || "No content ", 
+        createdAt: new Date().toDateString()
+    }
+    await redis.lpush(QUEUE_KEY, JSON.stringify(job));
+    
+})
