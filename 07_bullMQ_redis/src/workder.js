c51f71ece943285b5cb66794c999e2a57baf5e51
Ashish Patel <ashishpatel3630@gmail.com>
2026-08-31 15:29:35 +0530

    bullMQ

--- 07_bullMQ_redis/src/workder.js
+++ 07_bullMQ_redis/src/workder.js
@@ -1,8 +1,20 @@
-import { Worker } from "bullmq";
-import {connection} from "./queue";
+import { Job, Worker } from "bullmq";
+import { connection } from "./queue";
 
-const worker = new Worker(
-    'emails', 
-    console.log("server is running on port http://localhost:3000")
-    
-)
+const worker = new Worker("emails", async (Job) => {
+  (console.log("processing email.job ... ", Job.id, Job.name, Job.data),
+    await new Promise((resolve) => setTimeout(resolve, 1500)),
+    console.log("Email Job Completed !", Job.id, Job.name, Job.data));
+},
+{connection}
+);
+
+worker.on("completed", (Job) =>{
+    console.log("job completed!" ,Job.id ,Job.name, Job.data);
+
+});
+
+worker.on("failed", (Job , err)=>{
+    console.log("job failed! " , Job.id , Job.name , Job.data ,err);
+}
+);
