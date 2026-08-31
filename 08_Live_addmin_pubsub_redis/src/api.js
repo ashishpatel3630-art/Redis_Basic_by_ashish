fcdf2961281d5db67812f2fe225a5666e5ffb520
Ashish Patel <ashishpatel3630@gmail.com>
2026-08-31 17:03:13 +0530

    done with the pub sub in redis

--- 08_Live_addmin_pubsub_redis/src/api.js
+++ 08_Live_addmin_pubsub_redis/src/api.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { title } from "framer-motion/client";
+import { create } from "framer-motion/m";
 import Redis from "ioredis";
 
 const app = express();
@@ -7,7 +9,19 @@ app.use(express.json());
 
 const publisher = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
 
+app.post("/notification" , async(req , res)=>{
+    const payload = {
+        title : req.body.title || "Default Title",
+        createdAt : new Date().toISOString(),
+
+
+    }
+    const receivers  = await publisher.publish("notification",JSON.stringify(payload));
+    res.json({
+        message : `notification sent to ${receivers} subscribers`
+    });
+});
 
 app.listen(3000, ()=>{
-    
-})
+    console.log("Server is Running on port http://localhost:3000")
+});
