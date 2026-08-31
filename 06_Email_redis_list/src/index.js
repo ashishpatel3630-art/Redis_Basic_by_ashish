6650a91c792d37d858ee3e424c6bf666eb99e3df
Ashish Patel <ashishpatel3630@gmail.com>
2026-08-31 09:45:33 +0530

    emails is down

--- 06_Email_redis_list/src/index.js
+++ 06_Email_redis_list/src/index.js
@@ -1,30 +1,56 @@
-import express from "express"
-import { body } from "framer-motion/client";
-import Redis from "ioredis"
+import express from "express";
+import Redis from "ioredis";
 
 const app = express();
-app.use(express.json())
 
-const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379" );
+app.use(express.json());
 
-const QUEUE_KEY = 'queue:emails';
+const redis = new Redis(
+  process.env.REDIS_URL || "redis://localhost:6379"
+);
 
-app.post("/emails" , async( req , res)=>{
-    const job ={
-        to : req.body.to,
-        subject : req.body.subject || "No Subject",
-        body: req.body.body || "No content ", 
-        createdAt: new Date().toDateString()
-    }
-    await redis.lpush(QUEUE_KEY, JSON.stringify(job));
-    res.json({
-        queued : true ,
-        job
-    });
-    
+const QUEUE_KEY = "queue:emails";
+
+// Add email to queue
+app.post("/emails", async (req, res) => {
+  const job = {
+    to: req.body.to,
+    subject: req.body.subject || "No Subject",
+    body: req.body.body || "No content",
+    createdAt: new Date().toISOString(),
+  };
+
+  await redis.lpush(
+    QUEUE_KEY,
+    JSON.stringify(job)
+  );
+
+  res.json({
+    queued: true,
+    job,
+  });
 });
 
-app.get("/email/process-one", async( req , res)=>{
-    
+// Process one email job
+app.get("/email/process-one", async (req, res) => {
+  const rawJob = await redis.rpop(QUEUE_KEY);
 
+  if (!rawJob) {
+    return res.json({
+      message: "No jobs in the queue",
+    });
+  }
+
+  const job = JSON.parse(rawJob);
+
+  res.json({
+    message: "Email job processed",
+    job,
+  });
+});
+
+app.listen(3000, () => {
+  console.log(
+    "Server is running on http://localhost:3000"
+  );
 });
